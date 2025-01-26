2937b905c919e9fa3498e4e674d9e32b4745d780
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-26 17:10:19 +0600

    register async

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from 'src/prisma.service';
+import { faker } from '@faker-js/faker/.';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async register(dto: AuthDto) {
-    return { name: 'Yntymak' };
+    const oldUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (oldUser) throw new BadRequestException('User already exists');
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.person.firstName(),
+        avatarPath: faker.image.avatar(),
+        phone: faker.phone.number(),
+        password: await hash(dto.password),
+      },
+    });
+
+    return user;
   }
 }
 
