309def66d5d44721eceaeab7d8a7e2a85bd3a69b
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-26 14:31:55 +0600

    as

---
+++ prisma/migrations/20250126082608_first_migrate/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "create_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_at" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatar_path" TEXT NOT NULL DEFAULT '/uploads/default-avatar.png',
+    "phone" TEXT NOT NULL DEFAULT '',
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_name_key" ON "user"("name");
