bbf4e561f26f06888e1571d64f0b5805b1f64f66
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-12 23:35:48 +0600

    now i starting this old react native project)))

--- src/auth/decorators/auth-decorators.ts
+++ src/auth/decorators/auth-decorators.ts
@@ -1,8 +1,12 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { request } from 'http';
 
 export const CurrentUser = createParamDecorator(
 (   data: keyof User,ctx:ExecutionContext) => {
-        const requiest
+        const request = ctx,switchToHttp().getRequest()
+        const user = request.user
+        return data ? user[data] : user
+
     };
 )
